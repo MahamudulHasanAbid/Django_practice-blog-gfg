b682f8ffd1cecf9fa9a0fdb9c59143631cfe0c35
MahamudulHasanAbid <mdabid91221.kh@gmail.com>
2023-10-18 00:01:22 +0600

    initial commit

---
+++ post/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+from .models import Post
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'home.html', context) 
